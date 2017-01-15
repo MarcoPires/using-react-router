0e603a3f49fda53bef785b46e121ed072d5819ff
MarcoPires <marcofspires@gmail.com>
2017-01-15 16:08:58 +0000

    app bootstrap

---
+++ README.md
@@ -0,0 +1,53 @@
+# Using React Router
+
+Using React Router is an application based on the tutorial of [Tutsplus - How It Works: React Router](https://code.tutsplus.com/courses/how-it-works-react-router/).
+
+## Version
+1.0.0 - 15/01/2017
+
+## Tech
+
+React Flashcard App uses:
+* Html
+* CSS
+* React
+* React-Router
+
+
+## Requirements
+
+React Flashcard App needs:
+
+* react
+* react-dom
+* babelify
+* babel-preset-react
+* babel-preset-es2015
+* babel-register
+
+## Installation
+
+```
+# Go to the projecto folder
+$ cd /using-react-router
+$ npm install
+```
+
+## Start development environment
+
+```
+$ npm run start«
+```
+
+## References
+* [Tutsplus - How It Works: React Router](https://code.tutsplus.com/courses/how-it-works-react-router/)
+* [Original Github project - how-its-made-react-router](https://github.com/tutsplus/how-its-made-react-router/)
+
+
+## Contacts
+For more information feel free to contact me:
+e-mail : marcofspires@gmail.com
+
+## License
+
+See the [LICENSE](LICENSE) file for license rights and limitations (MIT).
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path              = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+	entry: {
+		main: './src/main.jsx'
+	},
+
+	output: {
+		path       : path.join(__dirname, 'build'),
+		filename   : '[name].js',
+		publicPath : '/', // ---> this allows loading deep-routes correctly
+	},
+
+	devtool: 'inline-source-map',
+
+	devServer:{
+		inline             : true,
+		historyApiFallback : true, // ---> this allows loading deep-routes correctly
+		stats              : 'minimal'
+	},
+
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+
+	module: {
+		loaders: [
+			{
+				test    : /\.(js|jsx)$/,
+				loader  : 'babel',
+				include : path.join(__dirname, 'src'),
+				query	: {
+					presets : ['latest', 'react'],
+					plugins : ['transform-decorators-legacy']
+				}
+			}, {
+				test   : /\.scss$/,
+				loader : ExtractTextPlugin.extract(['css', 'sass'])
+			}, {
+				test   : /\.json$/,
+				loader : 'json'
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('main.css'),
+		new HtmlWebpackPlugin({
+			title    : 'Using react router',
+			template : 'index.html',
+			inject   : true
+		})
+	]
+};
